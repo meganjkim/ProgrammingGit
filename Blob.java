b2c506fc9d18da79fe7aab52395065bade3d45d1
meganjkim <megankim1221@gmail.com>
2023-09-11 01:12:36 -0700

    added encryptPassword and byteToHex which generates SHA1 hash as string

--- Blob.java
+++ Blob.java
@@ -1,6 +1,7 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.*;
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Blob {
     public String readFromFile(String fileName) throws IOException {
@@ -11,6 +12,31 @@ public class Blob {
         }
         br.close();
         return str;
+    }
+
+    public static String encryptPassword(String str) {
+        String sha1 = "";
+        try {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(str.getBytes("UTF-8"));
+            sha1 = byteToHex(crypt.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return sha1;
+    }
+
+    private static String byteToHex(final byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash) {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
     }
 
 }
--- BlobTester.java
+++ BlobTester.java
@@ -4,5 +4,6 @@ public class BlobTester {
     public static void main(String[] args) throws IOException {
         Blob b = new Blob();
         System.out.println(b.readFromFile("README.md"));
+        System.out.println(b.encryptPassword(b.readFromFile("README.md")));
     }
 }
