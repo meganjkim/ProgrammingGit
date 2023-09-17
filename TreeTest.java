47dc726f941a63c4a70c485817ab7dff5ef53b56
Kian Sharifi <kiansharifi88@gmail.com>
2023-09-16 21:12:30 -0700

    Working TreeTest

--- TreeTest.java
+++ TreeTest.java
@@ -1,8 +1,10 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TreeTest {
 
@@ -11,6 +13,9 @@ public class TreeTest {
     @BeforeEach
     void setUp() {
         tree = new Tree();
+         // Create the "./objects/" directory if it doesn't exist
+         File objectsDir = new File("./objects");
+         objectsDir.mkdirs();
     }
 
     @Test
@@ -18,11 +23,12 @@ public class TreeTest {
         tree.add("Entry 1");
         tree.add("Entry 2");
 
-        assertEquals(2, tree.getBlobTree().size());
+        ArrayList<String> blobTree = tree.getBlobTree();
+        assertEquals(2, blobTree.size());
 
         // Test adding a duplicate entry
         tree.add("Entry 1");
-        assertEquals(2, tree.getBlobTree().size()); // Size should not change
+        assertEquals(2, blobTree.size());
     }
 
     @Test
@@ -30,37 +36,43 @@ public class TreeTest {
         tree.add("Entry 1");
         tree.add("Entry 2");
 
+        ArrayList<String> blobTree = tree.getBlobTree();
         tree.remove("Entry 1");
-        assertEquals(1, tree.getBlobTree().size());
+
+        assertEquals(1, blobTree.size());
 
         // Test removing a non-existing entry
         tree.remove("Entry 3");
-        assertEquals(1, tree.getBlobTree().size()); // Size should not change
+        assertEquals(1, blobTree.size());
     }
 
     @Test
     void testHashFromString() {
         String input = "Test String";
-        String expectedHash = "532eaabd9574880dbf76b9b8cc00832c20a6ec113";
+        String expectedHash = "a5103f9c0b7d5ff69ddc38607c74e53d4ac120f2";
 
         String hash = Tree.hashFromString(input);
         assertEquals(expectedHash, hash);
     }
 
     @Test
-    void testSave() throws IOException {
-        // Create and add some entries
+    void testSave() {
         tree.add("Entry 1");
         tree.add("Entry 2");
 
-        // Save the tree
-        tree.save();
+        try {
+            tree.save();
+            ArrayList<String> blobTree = tree.getBlobTree();
 
-        // Check if the file was created
-        File tempFile = new File("./objects/" + tree.hashFromString("Entry 1\nEntry 2"));
-        assertTrue(tempFile.exists());
+            // Check if the file was created
+            String hash = Tree.hashFromString("Entry 1\nEntry 2");
+            File tempFile = new File("./objects/" + hash);
+            assertTrue(tempFile.exists());
 
-        // Clean up: Delete the file
-        tempFile.delete();
+            // Clean up: Delete the file
+            tempFile.delete();
+        } catch (IOException e) {
+            fail("IOException occurred during save: " + e.getMessage());
+        }
     }
 }
